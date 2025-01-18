fe96e568ea369ee07329f4710f7629d34f9d0b04
Anshu Upadhyay <techanshuupadhyay@gmail.com>
2025-01-18 21:22:58 +0530

    CJP-01 Write a program to check if a string is a palindrom

---
+++ revisonJava/src/anshu/java/palindrome/Palindrome.java
@@ -0,0 +1,29 @@
+package anshu.java.palindrome;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+	public static void main(String[] args) {
+		
+		
+		Scanner sc=new Scanner(System.in);
+		
+		System.out.println("Enter String");
+		
+		String str=sc.next();
+		String org_str=str;
+		
+		String rev=" ";
+		
+		int len=str.length();
+		
+		for(int i=len-1;i>=0;i--)
+		{
+			rev=rev+str.charAt(i);
+		}
+		
+		System.out.println(rev);
+	}
+
+}
